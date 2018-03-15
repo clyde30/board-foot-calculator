e0a8313d1c19eb51d519c0c0081ffd6aab9bb4c5
Austin Jones <ajonestx@gmail.com>
2018-03-14 22:55:06 -0600

    build html input

---
+++ main.js
@@ -0,0 +1,21 @@
+const lengthInput = document.querySelector('[name=length]');
+const widthInput = document.querySelector('[name=width]');
+const thicknessInput = document.querySelector('[name=thickness]');
+const costInput = document.querySelector('[name=cost]');
+const total = document.querySelector('.total');
+
+
+function calculateBF() {
+    let length = lengthInput.value;
+    let width = widthInput.value;
+    let thickness = thicknessInput.value;
+    let cost = costInput.value;
+    let bf = (length * width * thickness) / 12;
+    total.innerHTML = "$" + bf * cost;
+}
+
+lengthInput.addEventListener('input', calculateBF);
+widthInput.addEventListener('input', calculateBF);
+thicknessInput.addEventListener('input', calculateBF);
+costInput.addEventListener('input', calculateBF);
+
